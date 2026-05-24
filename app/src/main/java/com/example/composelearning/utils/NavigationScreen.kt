193cabd388b1c94ea799e58521183471f228a6b9
Dev-Ankur <ankurtomar8@outlook.com>
2026-05-24 14:05:24 +0530

    Refactor navigation to support arguments and add ThirdScreen destination
    - Add `ThirdScreen` composable and define its route in the `NavigationScreen` sealed class.
    - Implement route arguments for `SecondScreen`, including a `createRoute` helper function and logic to extract navigation arguments.
    - Update `SecondScreen` to use `NavHostController` for navigation instead of legacy `Intent` transitions.
    - Implement backstack management in `SecondScreen` using `popUpTo` with `inclusive = true` when navigating to `ThirdScreen`.
    - Refactor `FirstScreen` and `SecondScreen` layouts to use centered `Column` components with `fillMaxSize`.
    - Update `AndroidManifest.xml` to set `NavigationViewActivity` as the application's launcher activity.

--- app/src/main/java/com/example/composelearning/utils/NavigationScreen.kt
+++ app/src/main/java/com/example/composelearning/utils/NavigationScreen.kt
@@ -3,7 +3,10 @@ package com.example.composelearning.utils
 sealed class NavigationScreen(val route: String) {
 
     object FirstScreen : NavigationScreen("firstscreen")
-    object SecondScreen : NavigationScreen("secondscreen")
+    object SecondScreen : NavigationScreen("{data}/secondscreen"){
+       fun createRoute(data:String) = "$data/secondscreen"
+    }
+    object ThirdScreen : NavigationScreen("thirdscreen")
 
 
 }
