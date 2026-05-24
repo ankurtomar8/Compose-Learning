976e48fbb3325e19c7d41c520f23af28eaedb451
Dev-Ankur <ankurtomar8@outlook.com>
2026-05-24 10:51:42 +0530

    Implement navigation boilerplate and placeholder screens
    - Add Material3 dependency to the application build configuration.
    - Define `FirstScreen` and `SecondScreen` composable components as navigation targets.
    - Create `NavigationScreen` sealed class to manage destination routes.
    - Implement `NavigationViewActivity` as the entry point for the navigation flow.

--- app/src/main/java/com/example/composelearning/navigation/screen/SecondScreen.kt
+++ app/src/main/java/com/example/composelearning/navigation/screen/SecondScreen.kt
@@ -1,11 +1,22 @@
 package com.example.composelearning.navigation.screen
 
+import android.app.Activity
+import android.content.Intent
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import com.example.composelearning.navigation.navigation.AnotherActivity
 
 
 @Composable
-fun SecondScreen(){
+fun SecondScreen(context: Activity){
 
-    Text(text = "Second Screen")
+    Button(onClick = {
+        val intent = Intent(context, AnotherActivity::class.java)
+        context.startActivity(intent)
+    })
+    {
+        Text(text = " Got to AnotherSecond Screen")
+    }
+    Text(text = "In Second Screen")
 }
