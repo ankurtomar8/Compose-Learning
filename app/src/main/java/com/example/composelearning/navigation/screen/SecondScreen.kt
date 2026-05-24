596456fb3f4d9fa520f012e85f21eef81acbb1c0
Dev-Ankur <ankurtomar8@outlook.com>
2026-05-24 14:05:30 +0530

    Refactor navigation to support arguments and add ThirdScreen destination
    - Add `ThirdScreen` composable and define its route in the `NavigationScreen` sealed class.
    - Implement route arguments for `SecondScreen`, including a `createRoute` helper function and logic to extract navigation arguments.
    - Update `SecondScreen` to use `NavHostController` for navigation instead of legacy `Intent` transitions.
    - Implement backstack management in `SecondScreen` using `popUpTo` with `inclusive = true` when navigating to `ThirdScreen`.
    - Refactor `FirstScreen` and `SecondScreen` layouts to use centered `Column` components with `fillMaxSize`.
    - Update `AndroidManifest.xml` to set `NavigationViewActivity` as the application's launcher activity.

--- app/src/main/java/com/example/composelearning/navigation/screen/SecondScreen.kt
+++ app/src/main/java/com/example/composelearning/navigation/screen/SecondScreen.kt
@@ -1,22 +1,40 @@
 package com.example.composelearning.navigation.screen
 
 import android.app.Activity
-import android.content.Intent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.Navigation
 import com.example.composelearning.navigation.navigation.AnotherActivity
+import com.example.composelearning.utils.NavigationScreen
 
 
 @Composable
-fun SecondScreen(context: Activity){
+fun SecondScreen(context: Activity,navHostController: NavHostController){
 
-    Button(onClick = {
-        val intent = Intent(context, AnotherActivity::class.java)
-        context.startActivity(intent)
-    })
-    {
-        Text(text = " Got to AnotherSecond Screen")
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = {
+            navHostController.navigate(NavigationScreen.ThirdScreen.route){
+                popUpTo(NavigationScreen.FirstScreen.route){
+                    inclusive = true // remove every back stack
+                    //      launchSingleTop = true // No repeated composable fun in stack
+                }
+            }
+        })
+        {
+            Text(text = " Got to AnotherSecond Screen")
+        }
+        Text(text = "In Second Screen")
     }
-    Text(text = "In Second Screen")
 }
